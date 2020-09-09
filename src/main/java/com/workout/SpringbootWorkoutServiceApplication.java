d08fcab6b6a4e45f3564ab331864228580c587de
NithyaR-scb <70639236+NithyaR-scb@users.noreply.github.com>
2020-09-10 00:24:08 +0530

    Update SpringbootWorkoutServiceApplication.java

--- src/main/java/com/workout/SpringbootWorkoutServiceApplication.java
+++ src/main/java/com/workout/SpringbootWorkoutServiceApplication.java
@@ -2,7 +2,7 @@ package com.workout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*testing*/
 @SpringBootApplication
 public class SpringbootWorkoutServiceApplication {
 
